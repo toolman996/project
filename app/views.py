41d9db4131e11eba2f087cce5addc88e189a1cf1
wei <1938785959@qq.com>
2020-06-27 18:54:33 +0800

    在分支dev中

--- app/views.py
+++ app/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render,HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('提交')
+
+
+def indexx(request):
+    print()
+    return HttpResponse('提交')
